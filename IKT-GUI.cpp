abd4a4d286a671d8f4b9bd0fec3b304ea36bd139
F100cTomas <145491617+F100cTomas@users.noreply.github.com>
2024-11-24 19:10:26 +0100

    Added code and description

--- IKT-GUI.cpp
+++ IKT-GUI.cpp
@@ -5,10 +5,15 @@
 #include <cstdint>
 #include <windows.h>
 #include <fstream>
+#include "resource.h"
+
+static constexpr DWORD mydwstyle = WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX;
 
 static HWND hwnd;
-static uint64_t width = 0, height = 0;
+static int64_t width = 0, height = 0;
 static RGBQUAD* imagedata = NULL;
+static HBITMAP imagebitmap = NULL;
+static bool menuredraw = false;
 
 static void openFile(const wchar_t* path);
 static LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
@@ -37,14 +42,19 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
 
     // Create the window.
 
+    int screenwidth  = GetSystemMetrics(SM_CXSCREEN);
+    int screenheight = GetSystemMetrics(SM_CYSCREEN);
+    RECT rect = {screenwidth / 2 - 150, screenheight / 2, screenwidth / 2 + 150, screenheight / 2};
+    AdjustWindowRect(&rect, mydwstyle, TRUE);
+
     hwnd = CreateWindowExW(
         WS_EX_ACCEPTFILES | WS_EX_APPWINDOW,                              // Optional window styles.
         CLASS_NAME,                     // Window class
         L"Image Viewer",
-        WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX,            // Window style
+        mydwstyle,            // Window style
 
         // Size and position
-        CW_USEDEFAULT, CW_USEDEFAULT, 300, 100,
+        rect.left, rect.top, rect.right - rect.left, rect.bottom - rect.top,
 
         NULL,       // Parent window    
         menu,       // Menu
@@ -81,41 +91,129 @@ bool endsWith(const wchar_t* str, const wchar_t* suffix)
         return false;
     return wcsncmp(str + lenstr - lensuffix, suffix, lensuffix) == 0;
 }
-static char readtxtbyte(std::ifstream& file) {
+static char readtxtbyte(char*& file) {
     char byte = 0;
     for (int i = 0; i < 8; ++i) {
-        char ch = file.get();
+        char ch = *(file++);
         if (ch != '0' && ch != '1') {
-            MessageBoxExW(NULL, L".txt files can only contain '0' and '1' characters.", L"Error", MB_OK | MB_ICONERROR, NULL);
-            return 0b11111111;
+            MessageBoxExW(NULL, L".txt files can only contain '0' and '1' characters.: ", L"Error", MB_OK | MB_ICONERROR, NULL);
+            return (char)0b11111111;
         }
         byte |= (ch == '1' ? 1 : 0) << (7 - i);
     }
     return byte;
 }
+static INT_PTR CALLBACK QueryDialogProc(HWND hwndDlg, UINT message, WPARAM wParam, LPARAM lParam) {
+    switch (message) {
+    case WM_INITDIALOG:
+        return TRUE;
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK) {
+            wchar_t buffer1[256], buffer2[256];
+            GetDlgItemText(hwndDlg, IDC_EDIT_INT1, buffer1, 256);
+            GetDlgItemText(hwndDlg, IDC_EDIT_INT2, buffer2, 256);
+            if (!(swscanf_s(buffer1, L"%lld", &width) == 1 && swscanf_s(buffer2, L"%lld", &height) == 1)) {
+                MessageBoxW(hwndDlg, L"Invalid input", L"Error", MB_OK | MB_ICONERROR);
+            }
+            EndDialog(hwndDlg, IDOK);
+            return TRUE;
+        }
+        break;
+    }
+    return FALSE;
+}
+static void CALLBACK readfileexCallback(DWORD dwErrorCode, DWORD dwNumberOfBytesTransfered, LPOVERLAPPED lpOverlapped) {
+    CloseHandle(lpOverlapped->hEvent);
+}
 static void openFile(const wchar_t* path)
 {
-    DialogBoxW(GetModuleHandle(NULL), MAKEINTRESOURCE(IDD_QUERY_DIALOG), hwndParent, QueryDialogProc);
+    // Open the file for reading asynchronously
+    HANDLE file = CreateFileW(path, FILE_GENERIC_READ, NULL, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL | FILE_FLAG_NO_BUFFERING | FILE_FLAG_OVERLAPPED, NULL);
+    // Get the file size
+    size_t fileSize;
+    {
+        LARGE_INTEGER fileSizeLargeInteger;
+        GetFileSizeEx(file, &fileSizeLargeInteger);
+        fileSize = fileSizeLargeInteger.QuadPart;
+    }
+    // Files are .txt and store bytes as sequences of '0' and '1', this unnecessarily increases the file size by a factor of 8
     if (!endsWith(path, L".txt")) {
         MessageBoxExW(NULL, L"Files other than .txt files aren't supported yet.", L"Error", MB_OK | MB_ICONERROR, NULL);
         return;
     }
-    std::ifstream file{ path };
-    file.seekg(0, std::ios::end);
-    size_t fileSize = file.tellg() / 8;
+    // Each pixel is 4 'bytes' and each 'byte' is 8 bytes. fileSize is now in pixels.
     if (fileSize % 32 != 0) {
         MessageBoxExW(NULL, L".txt files need to have a multiple of 32 characters.", L"Error", MB_OK | MB_ICONERROR, NULL);
+        CloseHandle(file);
         return;
     }
-    char* data = new char[fileSize];
-    file.seekg(0, std::ios::beg);
-    char* pd = data;
-    for (size_t i = 0; i < fileSize; i += 4) {
-        RGBQUAD rgb;
-        rgb.rgbRed   = readtxtbyte(file);
-        rgb.rgbGreen = readtxtbyte(file);
-        rgb.rgbBlue  = readtxtbyte(file);
-        (void)readtxtbyte(file);
+    fileSize /= 32;
+    // Querries for image size
+    {
+        int option;
+    retrypoint:
+        DialogBoxW(GetModuleHandle(NULL), MAKEINTRESOURCE(IDD_QUERY_DIALOG), hwnd, QueryDialogProc);
+        if (fileSize != width * height) {
+            option = MessageBoxExW(NULL, L"The size you entered doesn't match the file size, continue anyway?", L"Error", MB_ABORTRETRYIGNORE | MB_ICONERROR, NULL);
+            if (option == IDRETRY) goto retrypoint;
+            if (option == IDABORT) {
+                CloseHandle(file);
+                return;
+            }
+        }
+    }
+    // Allocates space for raw color data
+    {
+        BITMAPINFO bitmapinfo;
+        ZeroMemory(&bitmapinfo, sizeof(BITMAPINFO));
+        bitmapinfo.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
+        bitmapinfo.bmiHeader.biWidth = width;
+        bitmapinfo.bmiHeader.biHeight = -height;
+        bitmapinfo.bmiHeader.biPlanes = 1;
+        bitmapinfo.bmiHeader.biBitCount = 32;
+        bitmapinfo.bmiHeader.biCompression = BI_RGB;
+        imagebitmap = CreateDIBSection(NULL, &bitmapinfo, DIB_RGB_COLORS, reinterpret_cast<void**>(&imagedata), NULL, NULL);
+    }
+    // The entire file is read into data before it can be parsed
+    char* data = new char[fileSize * 32];
+    for (size_t i = 0; i < fileSize * 32; i++) data[i] = '0';
+    {
+        OVERLAPPED overlapped;
+        ZeroMemory(&overlapped, sizeof(OVERLAPPED));
+        overlapped.hEvent = CreateEventExW(NULL, NULL, CREATE_EVENT_MANUAL_RESET, EVENT_MODIFY_STATE | SYNCHRONIZE);
+        (void)ReadFileEx(file, data, fileSize * 32, &overlapped, readfileexCallback);
+        (void)WaitForSingleObjectEx(overlapped.hEvent, 200, TRUE);
+        CloseHandle(file);
+    }
+    // Reads only the data found in the file, the rest is filled with black. data is deleted as it is no longer needed.
+    {
+        size_t imageSize = width * height;
+        size_t loopCount = min(fileSize, imageSize);
+        char* current = data;
+        for (size_t i = 0; i < loopCount; i++) {
+            RGBQUAD rgb{};
+            rgb.rgbRed = readtxtbyte(current);
+            rgb.rgbGreen = readtxtbyte(current);
+            rgb.rgbBlue = readtxtbyte(current);
+            rgb.rgbReserved = NULL;
+            (void)readtxtbyte(current);
+            imagedata[i] = rgb;
+        }
+        for (size_t i = loopCount; i < imageSize; i++) {
+            imagedata[i] = { 0, 0, 0, NULL };
+        }
+        delete[] data;
+    }
+    // Adjusts the window to match the size of the image and redraws it.
+    {
+        RECT rect;
+        GetWindowRect(hwnd, &rect);
+        rect.right = rect.left + width;
+        rect.bottom = rect.top + height;
+        AdjustWindowRect(&rect, mydwstyle, TRUE);
+        MoveWindow(hwnd, rect.left, rect.top, rect.right - rect.left, rect.bottom - rect.top, FALSE);
+        InvalidateRect(hwnd, NULL, TRUE);
+        menuredraw = true;
     }
 }
 static wchar_t* openFileDialog() {
@@ -159,7 +257,7 @@ static LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM l
         switch (LOWORD(wParam)) {
         case 1:
         {
-            wchar_t* path = openFileDialog(hwnd);
+            wchar_t* path = openFileDialog();
             if (path != NULL) {
                 openFile(path);
                 delete path;
@@ -181,12 +279,14 @@ static LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM l
     {
         PAINTSTRUCT ps;
         HDC hdc = BeginPaint(hwnd, &ps);
-
-        // All painting occurs here, between BeginPaint and EndPaint.
-
-        FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
-
+        // SetGraphicsMode(hdc, GM_ADVANCED);
+        HDC image = CreateCompatibleDC(hdc);
+        HGDIOBJ old = SelectObject(image, imagebitmap);
+        BitBlt(hdc, 0, 0, width, height, image, 0, 0, SRCCOPY);
+        SelectObject(image, old);
+        DeleteDC(image);
         EndPaint(hwnd, &ps);
+        if (menuredraw) DrawMenuBar(hwnd);
     }
     return 0;
     default:
